9ec71a8e5a91093017c63ff54c749351ab9fafe4
Jason Mei <jason.mei403@gmail.com>
2018-06-25 03:23:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,20 +7,20 @@ class Artist
   end
 
   def add_song(song)
-    self.songs << song
+    @songs << song
     song.artist = self
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    self.songs << song
+    @songs << song
     song.artist = self
   end
 
   def songs
     Song.all.select { |song| song.artist == self }
   end
-  
+
   def self.song_count
     Song.all.count
   end
--- lib/author.rb
+++ lib/author.rb
@@ -24,5 +24,5 @@ class Author
   def self.post_count
     Post.all.count
   end
-  
+
 end
