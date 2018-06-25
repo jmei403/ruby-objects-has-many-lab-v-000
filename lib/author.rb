58ffd7dbeffcfa8e73ae09db8d314d8b6073f421
Jason Mei <jason.mei403@gmail.com>
2018-06-25 03:26:18 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,24 +3,21 @@ class Author
 
   def initialize(name)
     @name = name
-    @posts = []
   end
 
   def add_post(post)
-    @posts << post
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
     post.author = self
   end
 
   def posts
     Post.all.select { |post| post.author == self }
   end
-  
+
   def self.post_count
     Post.all.count
   end
