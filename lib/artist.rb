618e9d09e8afbda3fcdf1813c5997a91b894b83e
Jason Mei <jason.mei403@gmail.com>
2018-06-25 03:25:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,17 +3,14 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
-    @songs << song
     song.artist = self
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
-    @songs << song
     song.artist = self
   end
 
